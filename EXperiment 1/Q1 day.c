a462d223e53d12c397e4d5dfb1b4590599b28a54
suhanikumarijsr041-dot <suhanikumarijsr041@gmail.com>
2025-08-31 19:56:14 +0530

    Add files via upload

---
+++ EXperiment 1/Q1 day.c
@@ -0,0 +1,12 @@
+//Write a program to input two numbers and display their sum.
+#include<stdio.h>
+int main(){
+	int sum,a,b;
+	printf("Enter the number a");
+	scanf("%d",&a);
+	printf("enter the number b");
+	scanf("%d",&b);
+	printf("sum of two number %d/n",sum=a+b);
+	return 0;
+}
+
